bba99106837490ad5b0b1916627125e7fa881613
spe <sam.p.esler@gmail.com>
2023-02-07 18:04:43 +1100

    initial working version

--- src/main.rs
+++ src/main.rs
@@ -15,11 +15,13 @@ fn main () {
     println!("Problem:");
     println!("{:#?}", s);
 
-    let ec = sudoku_to_exact_cover(s);
+    let ec = sudoku_to_exact_cover(&s);
 
-    let solver = init_solver(&ec);
+    let mut solver = init_solver(&ec);
 
     let sol = solver.solve();
+
+    println!("Solution raw:\n{sol:#?}");
     
     let t = s.read_exact_cover(ec, sol);
 
@@ -36,8 +38,9 @@ impl Sudoku {
         return Self {
             grid: BufReader::new(fs::File::open(file_path).unwrap())
                 .lines()
-                .map(|row| row?.chars().collect::<Vec<_>>().iter()
-                     .map(|digit| digit.parse().unwrap()))
+                .map(|row| row.unwrap().chars().collect::<Vec<_>>().iter()
+                     .map(|digit| digit.to_digit(10).unwrap() as u8)
+                     .collect())
                 .collect()
         }
     }
@@ -46,13 +49,13 @@ impl Sudoku {
         let mut sol_grid = self.grid.clone();
         let parser = Regex::new(r"\w(\d)(\d)").unwrap();
         for idx in s {
-            let choice = ec.choices[idx].iter().map(|i| ec.items[*i]).collect();
+            let choice: Vec<Item>  = ec.choices[idx-1].iter().map(|i| ec.items[*i].clone()).collect();
             assert!(choice.len() == 4);
-            let caps = parser.captures(choice[0]).unwrap();
-            let i = caps.get(1).unwrap().as_str().parse().unwrap();
-            let j = caps.get(2).unwrap().as_str().parse().unwrap();
-            let caps2 = parser.captures(choice[1]);
-            let k = caps2.get(2).unwrap().as_str().parse().unwrap();
+            let caps = parser.captures(&choice[0]).unwrap();
+            let i = caps.get(1).unwrap().as_str().parse::<usize>().unwrap();
+            let j = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+            let caps2 = parser.captures(&choice[1]).unwrap();
+            let k = caps2.get(2).unwrap().as_str().parse::<u8>().unwrap();
             sol_grid[i][j] = k;
         }
         return Sudoku { grid: sol_grid };
@@ -78,8 +81,8 @@ struct ExactCoverSolver<'a> {
 type ExactCoverSolution = Vec<usize>;
 
 fn init_solver<'a>(e: &'a ExactCover) -> ExactCoverSolver<'a> {
-    let n_items = (e.name.len() - 1) as i32;
-    let name2idx = zip(e.name, 0..=n_items).collect::<HashMap<_, _>>();
+    let n_items = (e.items.len() - 1) as i32;
+    let name2idx = zip(e.items.clone(), 0..=n_items).collect::<HashMap<_, _>>();
 
     let mut llink: Vec<i32> = (0..n_items).collect();
     llink.insert(0, n_items);
@@ -87,17 +90,17 @@ fn init_solver<'a>(e: &'a ExactCover) -> ExactCoverSolver<'a> {
     let mut rlink: Vec<i32> = (1..=n_items).collect();
     rlink.push(0);
 
-    let mut prev = name2idx.clone();
+    let mut prev = zip(0..=n_items, 0..=n_items).collect::<HashMap<_, _>>();
     let mut count = n_items + 2;
     let mut ulink: Vec<i32> = vec![0; count as usize];
     let mut toplen: Vec<i32> = vec![0; count as usize];
-    for (r, row) in e.iter().enumerate() {
+    for (r, row) in e.choices.iter().enumerate() {
         let mut first_item = count;
-        for (c, val) in row.iter().enumerate() {
-            toplen[name2idx[val] as usize] += 1;
-            toplen.push(name2idx[val]);
-            ulink.push(prev[val]);
-            prev.insert(val, count);
+        for (c, &val) in row.iter().enumerate() {
+            toplen[val] += 1;
+            toplen.push(val as i32);
+            ulink.push(prev[&(val as i32)]);
+            prev.insert(val as i32, count);
             count += 1;
         }
         toplen.push(-(r as i32) - 1);
@@ -106,7 +109,7 @@ fn init_solver<'a>(e: &'a ExactCover) -> ExactCoverSolver<'a> {
     }
 
     for i in 1..=n_items {
-        ulink[i as usize] = prev[&e.name[i as usize]];
+        ulink[i as usize] = prev[&i];
     }
     let mut dlink: Vec<i32> = vec![0; ulink.len()];
     for (i, val) in ulink.iter().enumerate() {
@@ -123,6 +126,7 @@ fn init_solver<'a>(e: &'a ExactCover) -> ExactCoverSolver<'a> {
     }
     return ExactCoverSolver { 
         ec: e,
+        name: e.items.clone(),
         toplen: toplen,
         llink: llink, 
         rlink: rlink,
@@ -133,7 +137,7 @@ fn init_solver<'a>(e: &'a ExactCover) -> ExactCoverSolver<'a> {
 
 
 impl ExactCoverSolver<'_> {
-    fn solve(&self) -> ExactCoverSolution {
+    fn solve(&mut self) -> ExactCoverSolution {
         // X1
         let N = self.llink.len() - 1;
         let Z = self.toplen.len() - 1;
@@ -166,7 +170,7 @@ impl ExactCoverSolver<'_> {
                 // X8
                 if l == 0 {
                     println!("Failed");
-                    return x;
+                    return vec![];
                 }
                 else {
                     l -= 1;
@@ -211,10 +215,10 @@ impl ExactCoverSolver<'_> {
             }
             x[i] = -self.toplen[x[i] as usize];
         }
-        return x[..l].to_vec();
+        return x[..l].iter().map(|&item| item as usize).collect::<Vec<usize>>();
     }
 
-    fn cover(&self, i: i32) -> () {
+    fn cover(&mut self, i: i32) -> () {
         let mut p = self.dlink[i as usize];
         while p != i {
             self.hide(p);
@@ -226,7 +230,7 @@ impl ExactCoverSolver<'_> {
         self.llink[r as usize] = l;
     }
 
-    fn hide(&self, p: i32) -> () {
+    fn hide(&mut self, p: i32) -> () {
         let mut q = p + 1;
         while q != p {
             let x = self.toplen[q as usize];
@@ -244,7 +248,7 @@ impl ExactCoverSolver<'_> {
         }
     }
 
-    fn uncover(&self, i: i32) {
+    fn uncover(&mut self, i: i32) {
         let l = self.llink[i as usize];
         let r = self.rlink[i as usize];
         self.rlink[l as usize] = i;
@@ -256,7 +260,7 @@ impl ExactCoverSolver<'_> {
         }
     }
 
-    fn unhide(&self, p: i32) {
+    fn unhide(&mut self, p: i32) {
         let mut q = p - 1;
         while q != p {
             let x = self.toplen[q as usize];
@@ -275,11 +279,11 @@ impl ExactCoverSolver<'_> {
     }
 }
 
-fn sudoku_to_exact_cover(sudoku: Sudoku) -> ExactCover {
+fn sudoku_to_exact_cover(sudoku: &Sudoku) -> ExactCover {
     let mut exclude = HashMap::new();
     for i in 0..9 {
         for j in 0..9 {
-            let k = sudoku[i][j];
+            let k = sudoku.grid[i][j];
             let x = 3 * (i / 3) + (j / 3);
             if k != 0 {
                 exclude.insert(format!("p{i}{j}"), true);
@@ -289,7 +293,7 @@ fn sudoku_to_exact_cover(sudoku: Sudoku) -> ExactCover {
             }
         }
     }
-    let mut choices: ExactCover = Vec::new();
+    let mut choices = Vec::new();
     for i in 0..9 {
         for j in 0..9 {
             for k in 1..=9 {
@@ -318,9 +322,14 @@ fn sudoku_to_exact_cover(sudoku: Sudoku) -> ExactCover {
     }
     let mut name = names.keys().cloned().cloned().collect::<Vec<Item>>();
     name.insert(0, String::from(""));
+    let name2idx = zip(name.clone(), 0..name.len()).collect::<HashMap<_, _>>();
     println!("{:#?}", choices);
     return ExactCover {
         items: name,
-        choices: choices
+        choices: choices.iter()
+            .map(|row| row.iter()
+                 .map(|val| name2idx[val])
+                 .collect())
+            .collect()
     }
 }
